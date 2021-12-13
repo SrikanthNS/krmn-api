798d65fdadd08392b21582878ff9cfcca5aed669
Srikanth N S <7464926+SrikanthNS@users.noreply.github.com>
2021-12-14 01:14:45 +0530

    ui update

--- server.js
+++ server.js
@@ -109,11 +109,6 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to fullstack-server-side application." });
-});
-
 
 // Auth routes
 require('./app/routes/auth.routes')(app);
