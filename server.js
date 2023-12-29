fdc984048d823b25f5cec60e8067aebf034f1cf2
SrikanthNS <nssrikanth7@gmail.com>
2023-12-29 21:48:59 +0530

    add new views

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const cors = require("cors");
 var bcrypt = require("bcryptjs");
 require("dotenv").config();
 
-const path = __dirname + '/app/views/';
+const path = __dirname + "/app/views/";
 const app = express();
 
 app.use(express.static(path));
 
-
 var corsOptions = {
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:8081",
 };
 
 // parse requests of content-type - application/json
@@ -25,9 +24,7 @@ const Role = db.role;
 const User = db.user;
 const Client = db.client;
 
-
-db.sequelize.sync({ force: false })
-
+db.sequelize.sync({ force: false });
 
 // Think before you enable below code snippet
 // db.sequelize.sync({ force: true }).then(() => {
@@ -36,68 +33,73 @@ db.sequelize.sync({ force: false })
 // });
 
 function initial() {
-    Role.create({
-        id: 1,
-        name: "user"
-    });
+  Role.create({
+    id: 1,
+    name: "user",
+  });
 
-    Role.create({
-        id: 2,
-        name: "moderator"
-    });
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
 
-    Role.create({
-        id: 3,
-        name: "admin"
-    });
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
 
-    // Create User
-    User.create({
-        id: 1,
-        username: 'kishore',
-        email: 'kishore_ns@krmn.in',
-        password: bcrypt.hashSync('qwerty', 8)
-    }).then(user => {
-        user.setRoles(2);
-        user.setRoles(3);
-    }).catch(error => console.log("error::", error.message));
+  // Create User
+  User.create({
+    id: 1,
+    username: "kishore",
+    email: "kishore_ns@krmn.in",
+    password: bcrypt.hashSync("qwerty", 8),
+  })
+    .then((user) => {
+      user.setRoles(2);
+      user.setRoles(3);
+    })
+    .catch((error) => console.log("error::", error.message));
 
-    User.create({
-        id: 2,
-        username: 'manjunath',
-        email: 'manjunath@krmn.in',
-        password: bcrypt.hashSync('qwerty', 8)
-    }).then(user => {
-        user.setRoles(2);
-        user.setRoles(3);
-    }).catch(error => console.log("error::", error.message));
+  User.create({
+    id: 2,
+    username: "manjunath",
+    email: "manjunath@krmn.in",
+    password: bcrypt.hashSync("qwerty", 8),
+  })
+    .then((user) => {
+      user.setRoles(2);
+      user.setRoles(3);
+    })
+    .catch((error) => console.log("error::", error.message));
 
-    User.create({
-        id: 3,
-        username: 'Srikanth',
-        email: 'nssrikanth7@gmail.com',
-        password: bcrypt.hashSync('qwerty', 8)
-    }).then(user => {
-        user.setRoles(2);
-        user.setRoles(3);
-    }).catch(error => console.log("error::", error.message));
+  User.create({
+    id: 3,
+    username: "Srikanth",
+    email: "nssrikanth7@gmail.com",
+    password: bcrypt.hashSync("qwerty", 8),
+  })
+    .then((user) => {
+      user.setRoles(2);
+      user.setRoles(3);
+    })
+    .catch((error) => console.log("error::", error.message));
 
+  // create clients
+  Client.create({
+    id: 1,
+    name: "Client 1",
+  });
 
-    // create clients
-    Client.create({
-        id: 1,
-        name: "Client 1"
-    });
+  Client.create({
+    id: 2,
+    name: "Client 2",
+  });
 
-    Client.create({
-        id: 2,
-        name: "Client 2"
-    });
-
-    Client.create({
-        id: 3,
-        name: "Client 3"
-    });
+  Client.create({
+    id: 3,
+    name: "Client 3",
+  });
 }
 app.use(cors(corsOptions));
 
@@ -107,11 +109,10 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-
 // Auth routes
-require('./app/routes/auth.routes')(app);
+require("./app/routes/auth.routes")(app);
 // User routes
-require('./app/routes/user.routes')(app);
+require("./app/routes/user.routes")(app);
 
 // // Tutorial routes
 // require("./app/routes/tutorial.routes")(app);
@@ -120,17 +121,20 @@ require("./app/routes/task.routes")(app);
 // Client routes
 require("./app/routes/client.routes")(app);
 
-
 // Send index.html if no route Matches
-app.get('*', function (req, res) {
-    res.sendFile(path + "index.html", function (err) {
-        if (err) {
-            res.status(500).send(err)
-        }
-    })
+app.get("*", function (req, res) {
+  res.sendFile(path + "index.html", function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
+  console.log(
+    "🚀 ~ file: server.js:138 ~ app.listen ~ REACT_APP_API_BASE_URL:",
+    process.env.REACT_APP_API_BASE_URL
+  );
 });
