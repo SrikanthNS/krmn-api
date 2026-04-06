8a54c396ff6eee1980a47ad178d8e49dbd725a37
Srikanth NS <sns2@ford.com>
2026-04-06 13:01:23 +0530

    Handle expired session mainly access-token and refresh-token

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -4,7 +4,7 @@ if (!process.env.JWT_SECRET && process.env.NODE_ENV === "production") {
 
 module.exports = {
   secret: process.env.JWT_SECRET || "krmn-dev-secret-key",
-  jwtExpiration: Number.parseInt(process.env.JWT_EXPIRATION, 10) || 3600, // 1 hour
+  jwtExpiration: Number.parseInt(process.env.JWT_EXPIRATION, 10) || 86400, // 24 hours
   jwtRefreshExpiration:
-    Number.parseInt(process.env.JWT_REFRESH_EXPIRATION, 10) || 86400, // 24 hours
+    Number.parseInt(process.env.JWT_REFRESH_EXPIRATION, 10) || 172800, // 48 hours
 };
--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -76,7 +76,7 @@ exports.signin = async (req, res) => {
       }
 
       var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
+        expiresIn: config.jwtExpiration,
       });
 
       let refreshToken = await RefreshToken.createToken(user);
